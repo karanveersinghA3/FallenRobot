b3345f2b1da27c015f229f80ee22887fc5e9ff3a
Harpreet7382 <105529662+Harpreet7382@users.noreply.github.com>
2022-08-06 17:33:18 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -782,7 +782,7 @@ def main():
         try:
             dispatcher.bot.sendAnimation(
                 f"@{SUPPORT_CHAT}"",
-                animation="",
+                animation="https://telegra.ph/file/b360c49a906b06e459ec0.mp4",
                 caption=f"""
 ㅤㅤ🥀 {BOT_NAME} ɪs ᴀʟɪᴠᴇ ʙᴀʙʏ...
 
